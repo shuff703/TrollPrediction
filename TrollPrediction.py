55c1a8c416ed3a82953170fc9cb30f73078df75a
Scott Huff <shuff703@gmail.com>
2018-04-27 20:36:34 -0400

    Added cleaning code for the tweet data (need to pull down better tweets)

--- TrollPrediction.py
+++ TrollPrediction.py
@@ -1,6 +1,25 @@
 import pandas as pd
 import numpy as np
 import nltk
+import preprocessor as p
 
-troll_data = pd.read_csv('/Tweets.csv')
-real_data = pd.read_csv('/Tweets_Remainder')
+cols = [
+    'UserId',
+    'UserName',
+    'CreatedAt',
+    'RetweetCount',
+    'IsRetweet',
+    'FavoriteCount',
+    'Text',
+    'Id',
+    'Source',
+    'Hashtags',
+    'Urls',
+    'Posted',
+    'UserMentions'
+]
+real_data = pd.read_csv('Data/Tweets_Remainder.csv', names=cols, encoding='utf-8')
+for i, tweet in enumerate(real_data['Text']):
+    real_data.iloc(i).Text = p.clean(str(tweet).encode('ascii', errors='ignore').decode())
+
+print(real_data)
